04d6f2ee7668246dd31ec49d0e405f0722741c8f
TL <tom.kennedy.lee@gmail.com>
2019-05-18 10:09:53 -0400

    adding shakedown and updating files

---
+++ shakedown/data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One', 
+            'body': '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+            'author': 'Tom Lee',
+            'create_date': '05-18-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two', 
+            'body': '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+            'author': 'Ayrton Senna',
+            'create_date': '05-18-2019'   
+        }, 
+         {
+            'id': 3,
+            'title': 'Article Three', 
+            'body': '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+            'author': 'Ayrton Senna',
+            'create_date': '05-18-2019'   
+        }
+    ]
+    return articles
+    
